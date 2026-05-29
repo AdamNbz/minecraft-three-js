d6aa3fa7910618aefc67e8180797a7912635abcf
23521093 <156832544+hamstrous@users.noreply.github.com>
2026-05-29 19:29:13 +0700

    feat: load new texture

--- minecraft-js/Scripts/blocks.js
+++ minecraft-js/Scripts/blocks.js
@@ -16,59 +16,89 @@ const textures = {
     grassSide: loadTexture('textures/grass_side.png'),
     stone: loadTexture('textures/stone.png'),
     coal0re: loadTexture('textures/coal_ore.png'),
-    iron0re:loadTexture('textures/iron_ore.png')
+    iron0re:loadTexture('textures/iron_ore.png'),
+    leaves: loadTexture('textures/leaves.png'),
+    treeSide: loadTexture('textures/tree_side.png'),
+    treeTop: loadTexture('textures/tree_top.png'),
+    sand: loadTexture('textures/sand.png')
 };
 
 export const blocks = {
-    empty: {
-        id: 0,
-       name : 'empty'
-    },
-
-    grass: {
-        id: 1,
-        name: 'grass',
-        color: 0x559020,
-        material: [
-            new THREE.MeshLambertMaterial({ map: textures.grassSide }), //right
-            new THREE.MeshLambertMaterial({ map: textures.grassSide }), //left 
-            new THREE.MeshLambertMaterial({ map: textures.grass }), //top
-            new THREE.MeshLambertMaterial({ map: textures.dirt }), //bottom
-            new THREE.MeshLambertMaterial({ map: textures.grassSide }), // front
-            new THREE.MeshLambertMaterial({ map: textures.grassSide }) // back
-        ]
-    },
-    dirt: {
-        id: 2,
-       name: 'dirt',
-        color: 0x807020,
-        material: new THREE.MeshLambertMaterial({map:textures.dirt})
-    },
-    stone: {
-        id: 3,
-        name: 'stone',
-        color: 0x808080,
-        scale: { x: 30, y: 30, z: 30 },
-        scarity: 0.5,
-        material: new THREE.MeshLambertMaterial({map:textures.stone})
-    },
-    coal0re: {
-        id: 4,
-        name: 'coal0re',
-        color: 0x202020,
-        scale: { x: 20, y: 20, z: 20 },
-        scarity: 0.8,
-        material: new THREE.MeshLambertMaterial({ map: textures.coal0re })
-    },
-    iron0re: {
-        id: 5,
-        name: 'iron0re',
-        color: 0x806060,
-        scale: { x: 60, y: 60, z: 60 },
-        scarity: 0.9,
-        material: new THREE.MeshLambertMaterial({ map: textures.iron0re })
-    }       
-}
+  empty: {
+    id: 0,
+    name: 'empty',
+    visible: false
+  },
+  grass: {
+    id: 1,
+    name: 'grass',
+    material: [
+      new THREE.MeshLambertMaterial({ map: textures.grassSide }), // right
+      new THREE.MeshLambertMaterial({ map: textures.grassSide }), // left
+      new THREE.MeshLambertMaterial({ map: textures.grass }), // top
+      new THREE.MeshLambertMaterial({ map: textures.dirt }), // bottom
+      new THREE.MeshLambertMaterial({ map: textures.grassSide }), // front
+      new THREE.MeshLambertMaterial({ map: textures.grassSide })  // back
+    ]
+  },
+  dirt: {
+    id: 2,
+    name: 'dirt',
+    material: new THREE.MeshLambertMaterial({ map: textures.dirt })
+  },
+  stone: {
+    id: 3,
+    name: 'stone',
+    material: new THREE.MeshLambertMaterial({ map: textures.stone }),
+    scale: { x: 30, y: 30, z: 30 },
+    scarcity: 0.8
+  },
+  coalOre: {
+    id: 4,
+    name: 'coal_ore',
+    material: new THREE.MeshLambertMaterial({ map: textures.coalOre }),
+    scale: { x: 20, y: 20, z: 20 },
+    scarcity: 0.8
+  },
+  ironOre: {
+    id: 5,
+    name: 'iron_ore',
+    material: new THREE.MeshLambertMaterial({ map: textures.ironOre }),
+    scale: { x: 40, y: 40, z: 40 },
+    scarcity: 0.9
+  },
+  tree: {
+    id: 6,
+    name: 'tree',
+    visible: true,
+    material: [
+      new THREE.MeshLambertMaterial({ map: textures.treeSide }), // right
+      new THREE.MeshLambertMaterial({ map: textures.treeSide }), // left
+      new THREE.MeshLambertMaterial({ map: textures.treeTop }), // top
+      new THREE.MeshLambertMaterial({ map: textures.treeTop }), // bottom
+      new THREE.MeshLambertMaterial({ map: textures.treeSide }), // front
+      new THREE.MeshLambertMaterial({ map: textures.treeSide })  // back
+    ]
+  },
+  leaves: {
+    id: 7,
+    name: 'leaves',
+    visible: true,
+    material: new THREE.MeshLambertMaterial({ map: textures.leaves })
+  },
+  sand: {
+    id: 8,
+    name: 'sand',
+    visible: true,
+    material: new THREE.MeshLambertMaterial({ map: textures.sand })
+  },
+  cloud: {
+    id: 9,
+    name: 'cloud',
+    visible: true,
+    material: new THREE.MeshBasicMaterial({ color: 0xf0f0f0 })
+  }
+};
 
 export const resources = [
         blocks.stone,
